25e598f8b55b772bd5b95287696ec0983210a406
unknown <puchi36452@gmail.com>
2020-01-28 22:42:10 -0600

    Adding files for github pages-monster-rolodex

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const SearchBox = ({placeholder,handleChange})=>{
+    return(
+        <input type="search" placeholder={placeholder} onChange={handleChange}/>
+    );
+};
